d4349a65aae34f0daec8301b3e50d774c8a6a9a3
Quentin Billac <quentin.billac2@gmail.com>
2022-04-15 11:19:50 +0200

    ajout du timer

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,16 +1,46 @@
-import { useState } from "react";
-
-export default function Counter() {
-  const [count, setCount] = useState(0);
-
+function Counter({
+  timerYears,
+  timerDays,
+  timerHours,
+  timerMinutes,
+  timerSecondes,
+}) {
   return (
-    <p>
-      <button
-        type="button"
-        onClick={() => setCount((oldCount) => oldCount + 1)}
-      >
-        count is: {count}
-      </button>
-    </p>
+    <section className="timer_center_qb">
+      <section className="timer_parent_qb">
+        <div className="timer_clock_qb">
+          <section className="timer_text_qb">
+            <p>{timerDays}</p>
+            <small>Jours</small>
+          </section>
+          <section className="timer_text_qb">
+            <p>{timerHours}</p>
+            <small>Heures</small>
+          </section>
+          <div className="timer_sup_qb">
+            <p>{timerYears}</p>
+            <small>Ans</small>
+          </div>
+          <section className="timer_text_qb">
+            <p>{timerMinutes}</p>
+            <small>Minutes</small>
+          </section>
+          <section className="timer_text_qb">
+            <p>{timerSecondes}</p>
+            <small>Secondes</small>
+          </section>
+        </div>
+      </section>
+    </section>
   );
 }
+
+Counter.defaultProps = {
+  timerYears: 0,
+  timerDays: 0,
+  timerHours: 0,
+  timerMinutes: 0,
+  timerSecondes: 0,
+};
+
+export default Counter;
