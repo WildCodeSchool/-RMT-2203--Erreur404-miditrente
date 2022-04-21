00a044c60eb477d253b7f4df7a58a4b5dc1f8966
Marie_Tignol <marie-tignol_student2022@wilder.school>
2022-04-22 00:09:11 +0200

    files nav.jsx + nav.css modified

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -8,37 +8,43 @@ function Nav() {
 
   const handleShowLinks = () => setShowLinks(!showlinks);
   return (
-    <nav className={`navbar_mt ${showlinks ? "show_nav_mt" : "hidden_na_mt"}`}>
-      <div calssName="navbar_logo_mt">Logo</div>
+    <nav className={`navbar_mt ${showlinks ? "show_nav_mt" : "hidden_nav_mt"}`}>
+      <div className="navbar_logo_mt" />
+      <Link to="/" className="nav_logo_mt">
+        Logo
+      </Link>
       <ul className="navbar_links_mt">
         <li className="navbar_item_mt">
-          <Link to="/" className="navbar_link_mt">
-            {" "}
-            Accueil{" "}
+          <Link to="/" className="navbar_link_mt" onClick={handleShowLinks}>
+            Accueil
           </Link>
         </li>
         <li className="navbar_item_mt">
-          <Link to="/Info" className="navbar_link_mt">
-            {" "}
-            A propos{" "}
+          <Link to="/Info" className="navbar_link_mt" onClick={handleShowLinks}>
+            A propos
           </Link>
         </li>
         <li className="navbar_item_mt">
-          <Link to="/Co2" className="navbar_link_mt">
-            {" "}
-            Concentration Co2{" "}
+          <Link to="/Co2" className="navbar_link_mt" onClick={handleShowLinks}>
+            Concentration Co2
           </Link>
         </li>
         <li className="navbar_item_mt">
-          <Link to="/Fonte" className="navbar_link_mt">
-            {" "}
-            Fonte des glaces{" "}
+          <Link
+            to="/Fonte"
+            className="navbar_link_mt"
+            onClick={handleShowLinks}
+          >
+            Fonte des glaces
           </Link>
         </li>
         <li className="navbar_item_mt">
-          <Link to="/Contact" className="navbar_link_mt">
-            {" "}
-            Contact{" "}
+          <Link
+            to="/Contact"
+            className="navbar_link_mt"
+            onClick={handleShowLinks}
+          >
+            Contact
           </Link>
         </li>
       </ul>
