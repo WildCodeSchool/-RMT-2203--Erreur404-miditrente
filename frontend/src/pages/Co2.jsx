f06d5fbfb2734b95aa220a5c46f90a0d245a96a9
Terry Grimoire <terrt.grimoire@gmail.com>
2022-04-24 00:15:48 +0200

    Adding props to the component explication and creating the data file for explicationsdata

--- frontend/src/pages/Co2.jsx
+++ frontend/src/pages/Co2.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import Explication from "../components/Explication";
+import explicationData from "../datas/explication-data";
 
 import "../CO2.css";
 
@@ -7,8 +9,8 @@ function Co2() {
     <div className="co2_main_tg">
       <h1>Concentration de Co2</h1>
       <div className="co2_graph_tg" />
-      <Explication />
-      <Explication />
+      <Explication explicationData={explicationData[1]} />
+      <Explication explicationData={explicationData[2]} />
     </div>
   );
 }
