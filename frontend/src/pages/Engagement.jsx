c4c5740145912b27be85c0f4d94455ed2fe2e846
Terry Grimoire <terrt.grimoire@gmail.com>
2022-04-24 03:56:49 +0200

    creation of the component engagement and css file and also the engagement data and props

--- frontend/src/pages/Engagement.jsx
+++ frontend/src/pages/Engagement.jsx
@@ -5,11 +5,12 @@ import engagementData from "../datas/engagement-data";
 import "./Engagement.css";
 
 function Engagement() {
-
   return (
     <div className="engagement_main_tg">
       <h1 className="engagement_h1_tg"> Agir maintenant </h1>
-      { engagementData.map((data) => <Actions data={data} />)} 
+      {engagementData.map((data) => (
+        <Actions data={data} />
+      ))}
     </div>
   );
 }
