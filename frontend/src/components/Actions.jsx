a54ab1918bd3860b805f27d92284949f4652e2ba
Terry Grimoire <terrt.grimoire@gmail.com>
2022-04-25 00:58:15 +0200

    starting the engagement page with all the components based on a data file using props and useState

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Actions({ data }) {
+  const [likes, setLikes] = useState(data.likes);
+  const [buttonClass, setButtonClass] = useState("actions_bouton_tg");
+
+  const handleClick = () => {
+    setLikes((like) => like + 1);
+    setButtonClass(() => `actions_new_bouton_tg`);
+  };
+
   return (
     <div className="actions_container_tg">
       <h3 className="actions_EngagementType_tg">{data.text}</h3>
-      <h3 className="actions_bouton_tg">
-        Je m&apos;engage <span> ♥ {data.likes}</span>
-      </h3>
+      <button type="button" className={buttonClass} onClick={handleClick}>
+        Je m&apos;engage <span> ♥ {likes}</span>
+      </button>
     </div>
   );
 }
--- frontend/src/pages/Engagement.jsx
+++ frontend/src/pages/Engagement.jsx
@@ -5,12 +5,18 @@ import engagementData from "../datas/engagement-data";
 import "./Engagement.css";
 
 function Engagement() {
+  // const [dataTable, setDataTable] = useState(engagementData);
+
   return (
     <div className="engagement_main_tg">
       <h1 className="engagement_h1_tg"> Agir maintenant </h1>
-      {engagementData.map((data) => (
-        <Actions data={data} />
-      ))}
+      {engagementData
+        .sort((a, b) => {
+          return b.likes - a.likes;
+        })
+        .map((data) => (
+          <Actions data={data} />
+        ))}
     </div>
   );
 }
