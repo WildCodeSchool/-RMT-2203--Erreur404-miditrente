914a662f00e75be12337c0a1cfa6de67c1ac1bc2
Terry Grimoire <terrt.grimoire@gmail.com>
2022-04-26 02:07:49 +0200

    adding data in the engagement data file + adding a new button with more information about the engagement

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -5,6 +5,9 @@ function Actions({ data }) {
   const [buttonClass, setButtonClass] = useState("actions_bouton_tg");
   const [buttonActive, setButtonActive] = useState(true);
   const [engaged, setEngaged] = useState("Je m'engage");
+  const [whyengaged, setWhyEngaged] = useState("Pourquoi s'engager ?");
+  const [engagedActive, setEngagedActive] = useState(true);
+  const [engagedClass, setEngagedClass] = useState("actions_main_tg");
 
   const handleClick = () => {
     setButtonActive(!buttonActive);
@@ -12,18 +15,40 @@ function Actions({ data }) {
       setLikes((like) => like + 1);
       setButtonClass(() => `actions_new_bouton_tg`);
       setEngaged(() => "personnes engagées");
+      setWhyEngaged(() => "Merci de contribuer au sauvetage de notre planète");
     } else {
       setLikes((like) => like - 1);
       setButtonClass(() => `actions_bouton_tg`);
       setEngaged(() => "Je m'engage");
+      setWhyEngaged(() => "Pourquoi s'engager ?");
+    }
+  };
+
+  const handleClickWhy = () => {
+    setEngagedActive(!engagedActive);
+    if (engagedActive === true) {
+      setWhyEngaged(() => data.explication);
+      setEngagedClass(() => "actions_new_main_tg");
+    } else {
+      setWhyEngaged(() => "Pourquoi s'engager ?");
+      setEngagedClass(() => "actions_main_tg");
     }
   };
 
   return (
-    <div className="actions_container_tg">
-      <h3 className="actions_EngagementType_tg">{data.text}</h3>
-      <button type="button" className={buttonClass} onClick={handleClick}>
-        {engaged} <span> ♥ {likes}</span>
+    <div className={engagedClass}>
+      <section className="actions_container_tg">
+        <h3 className="actions_engagementType_tg">{data.text}</h3>
+        <button type="button" className={buttonClass} onClick={handleClick}>
+          {engaged} <span> ♥ {likes}</span>
+        </button>
+      </section>
+      <button
+        type="button"
+        className="actions_engagementExplanation_tg"
+        onClick={handleClickWhy}
+      >
+        {whyengaged}
       </button>
     </div>
   );
--- frontend/src/datas/engagement-data.js
+++ frontend/src/datas/engagement-data.js
@@ -4,40 +4,65 @@ const engagementData = [
   {
     text: "Je m'engage à éteindre la lumière",
     likes: 268,
-    category: "Premier niveau",
+    category: 1,
+    explication:
+      "On a tous vu cette publicité : « C’est pas Versailles ici ! ». Eh bien oui, éteindre la lumière, c’est bon pour votre budget et pour la planète. Même si vous habitez à Versailles  D’ailleurs, Louis XIV ne s’éclairait pas grâce à l’électricité mais à la bougie. Pensez-y :) ",
   },
   {
     text: "Je m'engage à trier mes déchets",
     likes: 153,
-    category: "Premier niveau",
+    category: 1,
+    explication:
+      "Trier ses déchets est la base d’une maison propre et organisée. Elle permet également de recycler les déchets et d’économiser des ressources naturelles. En effet, en triant correctement ses déchets, les emballages et les verres sont réutilisés pour produits de nouveaux objets en emballages. Cela éviter ainsi de puiser dans les ressources naturelles de la Terre. ",
   },
   {
     text: "Je m'engage à éteindre mes appareils éléctriques",
     likes: 94,
+    category: 1,
+    explication:
+      "Un appareil mis en veille consomme quand même de l’électricité. Ils peuvent représenter jusqu’à 10% de votre facture. Afin de faire des économies et limiter votre consommation, débrancher tout simplement vos appareils lorsqu’ils ne servent plus : grille-pain, bouilloire, cafetière ou encore chargeur de téléphone. Pour vous faciliter la vie, optez pour des multiprises avec des boutons marche/arrêt. En un seul geste, vous éteindrez et allumerez tous vos appareils.",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: "Je m'engage à limiter le chauffage et la climatisation ",
     likes: 85,
+    category: 1,
+    explication:
+      "Que ce soit en hiver ou en été, on a souvent tendance à en faire trop. Inutile de mettre le chauffage à fond dans votre chambre sachant que la température idéale se situe vers 17°C. En hiver, baisser la température de votre chauffage de 1°C et vous pourrez déjà constater la différence sur votre facture. En été, c’est la même chose. Si vous utilisez une climatisation, ne mettez pas trop froid. En plus de risquer d’attraper un rhume si vous sortez pendant la canicule après être resté dans une maison fraîche, votre facture sera aussi salée qu’en hiver. Et pour éviter les déperditions de chaleur, n’oubliez pas de baissez les volets. ",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: " Je m'engage à boire l’eau du robinet ",
     likes: 7,
+    category: 1,
+    explication:
+      "En France, nous avons la chance extrême d’avoir une eau potable dans tous nos robinets. À quoi bon dépenser de l’argent dans des bouteilles en plastiques alors que vous avez de l’eau directement chez vous ? Si vous craignez les éventuels défauts de goûts, vous pouvez investir dans une fontaine à eau, dans des filtres à charbons ou tout simplement dans une carafe filtrante.",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: "Je m'engage à mettre un couvercle sur les casseroles ",
     likes: 6,
+    category: 1,
+    explication:
+      "En cuisine, faire bouillir de l’eau peut parfois prendre du temps. Et ça consomme. Pour accélérer le processus d’ébullition, passer moins de temps et cuisine et réduire votre consommation d’énergie, pensez tout simplement à poser un couvercle sur vos casseroles. ",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: "Je m'engage à manger moins de viande ",
     likes: 6,
+    category: 2,
+    explication:
+      "En France 15 000 litres d’eau sont nécessaires pour produite 1kg de viande bovine (eau bue par l’animal et eau qui sert à la production de la nourriture pour cet animal). Pour 1 kg de porc, c’est 6000 litres. Pour 1 kg de poulet, c’est 4 000. Pour les légumineuses, les fruits et les légumes demandent nettement moins d’eau. Pour faire un petit geste, remplacer plusieurs fois dans la semaine des plats habituellement avec viande par des repas végétariens. Par exemple, tentez des lasagnes végétariennes (courgettes et aubergine) au lieu des lasagnes bolognaises, des steaks de sojas et de betterave au lieu du steak de bœuf, des hachis Parmentier aux légumes, des tajines aux lentilles et au riz, croquette-monsieur aux épinards et à la mozzarella, un chili végétarien ou encore des fajitas à l’avocat et aux haricots rouge. ",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: "Je m'engage à adopter des ampoules LED ",
     likes: 3,
+    category: 2,
+    explication:
+      "Bien qu’elles coûtent plus cher à l’achat que des ampoules classiques, les LED consomment 80% d’électricité en moins et ont une durée de vie plus longue. Vous pouvez même recevoir des ampoules gratuites sur www.mesapoulesgratuites.fr ",
   },
   {
-    text: "Je m'engage à éteindre la lumière",
+    text: "Je m'engage à faire des lessives à basse température",
     likes: 1,
+    category: 2,
+    explication:
+      "Fini l’époque où le lavage à basse température laissait des tâches sur vos vêtements ! Aujourd'hui, les lessives sont très efficaces (même celles faite maison). 80% de l’électricité utilisée par la machine à laver sert à faire chauffer l’eau. Alors essayez de privilégier des lavages à 30°C ou 40°C au lieu des habituels 60°C. ",
   },
 ];
 
