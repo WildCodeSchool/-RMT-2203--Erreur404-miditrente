8db01068e1006d13818997221f3bd5b5fb9dcafa
Terry Grimoire <terrt.grimoire@gmail.com>
2022-04-27 17:27:41 +0200

    changing the research bar to make it nicer

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -2,26 +2,16 @@ import React from "react";
 
 function Filter({ value, handleValue }) {
   return (
-    <div className="filter_main_tg">
-      <label htmlFor="search">
-        <input
-          type="text"
-          id="search"
-          value={value}
-          placeholder="Rechercher"
-          className="filter_input_tg"
-          onChange={(event) => handleValue(event.target.value)}
-        />
-      </label>
-
-      <label htmlFor="categorie">
-        <select name="categorie" id="categorie" className="filter_select_tg">
-          <option value="1">Facile</option>
-          <option value="2">Intermédiaire</option>
-          <option value="3">Expert</option>
-        </select>
-      </label>
-    </div>
+    <label htmlFor="search" className="filter_label_tg">
+      <input
+        type="text"
+        id="search"
+        value={value}
+        placeholder="Rechercher"
+        className="filter_input_tg"
+        onChange={(event) => handleValue(event.target.value)}
+      />
+    </label>
   );
 }
 
