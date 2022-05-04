9e9ba2ece01af8977f8f02685a391df29f5cbd0e
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-05-05 00:14:48 +0200

    CREATION css faq + réglages

--- frontend/src/components/FaqPage.jsx
+++ frontend/src/components/FaqPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
-import "../pages/Engagement.css";
-// import planetfaq from "../assets/planetfaq.png";
+import "../pages/Faq.css";
+import planetfaq from "../assets/planetfaq.png";
 /**
  *
  * @returns FAQ content
@@ -10,13 +10,15 @@ import "../pages/Engagement.css";
 function FaqPage() {
   const [faqs, setFaqs] = useState([
     {
-      question: "question 1",
-      answer: "réponse 1",
-      open: true,
+      question: "C'est quoi une limite planétaire?",
+      answer:
+        "Les limites planétaires sont les seuils que l'humanité ne devrait pas dépasser pour ne pas compromettre les conditions favorables dans lesquelles elle a pu se développer et pour pouvoir durablement vivre dans un écosystème sûr, c’est-à-dire en évitant les modifications brutales et difficilement prévisibles de l'environnement planétaire. Ce concept a été proposé par une équipe internationale de vingt-six chercheurs et publié en 2009. Il a depuis été mis à jour par des publications régulières.",
+      open: false,
     },
     {
-      question: "question 2",
-      answer: "réponse 2",
+      question: "Combien de limites planétaires existe-t-il?",
+      answer:
+        "Il existe neuf processus retenus comme limites, dont huit sont chiffrés par les chercheurs et six sont déjà franchies (la 6e a été franchie en 2022).",
       open: false,
     },
     {
@@ -45,34 +47,37 @@ function FaqPage() {
   return (
     <div>
       <div>
-        <div className="engagement_main_tg">
+        <div className="engagement_main_dd">
           {faqs.map((faq, index) => (
-            // <div>
-            // </div> nouvelle div: display flex pour la colonne à côté de l'image
-            // ajouter un pointeur sur les boutons
-            <div
-              className="actions_container_tg"
-
-              // style={{
-              //   backgroundColor: "white",
-              //   color: "crimson",
-              //   marginBottom: "10px",
-              // }}
-            >
-              <div
-                className="actions_engagementType_tg"
-                role="presentation"
-                onClick={() => handleChange(index)}
-                onKeyPress={() => handleChange(index)}
-              >
-                {faq.question}
+            <div className="actions_main_dd">
+              <div className="actions_container_dd">
+                <div className="actions_engagementType_dd">{faq.question}</div>
+                <div className="actions_div_dd" prout="image">
+                  {faq.open === true ? (
+                    <img
+                      src={planetfaq}
+                      alt="planet button"
+                      role="presentation"
+                      onClick={() => handleChange(index)}
+                      onKeyPress={() => handleChange(index)}
+                      className="actions_engagement_like_button_dd2"
+                    />
+                  ) : (
+                    <img
+                      src={planetfaq}
+                      alt="planet button"
+                      role="presentation"
+                      onClick={() => handleChange(index)}
+                      onKeyPress={() => handleChange(index)}
+                      className="actions_engagement_like_button_dd"
+                    />
+                  )}
+                </div>
               </div>
-              <div className="actions_engagementExplanation_tg">
+              <div className="actions_engagementExplanation_dd">
                 {faq.open === true ? faq.answer : ""}
               </div>
             </div>
-            // image avec le onclick, 2 images en ternaire
-            // fin div
           ))}
         </div>
       </div>
--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -2,8 +2,8 @@ import FaqPage from "@components/FaqPage";
 
 export default function FaqList() {
   return (
-    <div style={{ padding: "200px" }}>
-      <h1>Ici, le plus beau des titres.</h1>
+    <div className="foireAuxQuestions">
+      <h1>EARTH KEEPER - Foire aux Questions</h1>
       <FaqPage />
     </div>
   );
