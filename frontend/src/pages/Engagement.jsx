a16e3911eaee6c664a19485276d791c22ea9335d
Marie_Tignol <marie-tignol_student2022@wilder.school>
2022-05-09 18:50:54 +0200

    add filter and map function to engagement page

--- frontend/src/pages/Engagement.jsx
+++ frontend/src/pages/Engagement.jsx
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 import Actions from "../components/Actions";
 import Filter from "../components/Filter";
 import engagementData from "../datas/engagement-data";
+import Button from "../components/Button";
 
 import "./Engagement.css";
 
 function Engagement() {
   const [value, setValue] = useState("");
+  const [selection, setSelection] = useState(engagementData);
+  const categories = ["Tout", "Facile", "Intermediaire", "Expert"];
+  const handleClick = (button) => {
+    if (button === "Tout") {
+      setSelection(engagementData);
+      return;
+    }
+    const filteredData = engagementData.filter(
+      (data) => data.category === button
+    );
+    setSelection(filteredData);
+  };
 
   return (
     <div className="engagement_main_tg">
       <h1 className="engagement_h1_tg"> Agir maintenant </h1>
+      <Button categories={categories} handleClick={handleClick} />
       <Filter value={value} handleValue={setValue} />
-      {engagementData
+      {selection
         .filter((name) => name.text.includes(value))
         .sort((a, b) => {
           return b.likes - a.likes;
@@ -20,6 +34,7 @@ function Engagement() {
         .map((data) => (
           <Actions data={data} />
         ))}
+      );
     </div>
   );
 }
